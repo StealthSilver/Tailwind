0318e7352aec0ed4eba2dd6b306b1bc1024ec30d
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-09-23 13:51:07 +0530

    inter

--- 01-basics/my-app/components/navbar.tsx
+++ 01-basics/my-app/components/navbar.tsx
@@ -1,5 +1,33 @@
 import React from "react";
 
 export const Navbar = () => {
-  return <div>Navbar</div>;
+  const links = [
+    {
+      title: "Guide",
+      href: "#",
+    },
+
+    {
+      title: "Pricing",
+      href: "#",
+    },
+
+    {
+      title: "Login",
+      href: "#",
+    },
+  ];
+
+  return (
+    <div className="navbar-root">
+      <div className="logo">Finance</div>
+      <div>
+        {links.map((link) => (
+          <a key={link.title} href={link.href}>
+            {link.title}
+          </a>
+        ))}
+      </div>
+    </div>
+  );
 };
