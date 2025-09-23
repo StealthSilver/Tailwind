46490c2e8b64422a4d95766988ebb7b7813c00d4
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-09-23 15:07:14 +0530

    inter

--- 01-basics/my-app/components/Hero.tsx
+++ 01-basics/my-app/components/Hero.tsx
@@ -1,5 +1,28 @@
 import React from "react";
 
 export const Hero = () => {
-  return <div className="badge">What are form-199 filing requirements</div>;
+  return (
+    <div className="hero-root">
+      <div className="badge">
+        <span> What are form-199 filing requirements ?</span>
+
+        <svg
+          viewBox="0 0 16 16"
+          width="30"
+          height="30"
+          fill="none"
+          className="badge-icon"
+        >
+          <path
+            stroke="#1e1f25"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeOpacity="0.5"
+            strokeWidth="1"
+            d="M8 4.75 11.25 8m0 0L8 11.25M11.25 8h-6.5"
+          ></path>
+        </svg>
+      </div>
+    </div>
+  );
 };
