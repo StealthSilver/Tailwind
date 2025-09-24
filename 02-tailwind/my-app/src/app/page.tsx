0fba9f7c9d162ea96f81837a5003aedec9eaf481
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-09-24 14:37:25 +0530

    latest next version

--- 02-tailwind/my-app/src/app/page.tsx
+++ 02-tailwind/my-app/src/app/page.tsx
@@ -6,21 +6,20 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="h-screen relative [background:radial-gradient(125%_100%_at_50%_0%,_#FFF_6.32%,_#E0F0FF_29.28%,_#E7EFFD_68.68%,_#FFF_100%)]">
-      <div className="max-w-7xl mx-auto absolute inset-0 h-full w-full">
-        {/* left and right lines */}
-        <div className="absolute inset-y-0 left-0 h-full w-px bg-gradient-to-b from-neutral-300/50 via-neutral-200 to-transparent pointer-events-none"></div>
-        <div className="absolute inset-y-0 right-0 h-full w-px bg-gradient-to-b from-neutral-300/50 via-neutral-200 to-transparent pointer-events-none"></div>
-      </div>
-      <Container className="">
-        <Navbar></Navbar>
-        <Hero></Hero>
+      {/* left and right lines should be relative to the full screen, not container */}
+      <div className="absolute inset-y-0 left-0 h-full w-px bg-gradient-to-b from-neutral-300/50 via-neutral-200 to-transparent pointer-events-none"></div>
+      <div className="absolute inset-y-0 right-0 h-full w-px bg-gradient-to-b from-neutral-300/50 via-neutral-200 to-transparent pointer-events-none"></div>
+
+      <Container className={""}>
+        <Navbar />
+        <Hero />
       </Container>
 
-      {/* horizontail line */}
-
-      <div className="relativ w-full">
+      {/* horizontal line + banner */}
+      <div className="relative w-full mt-12">
+        {" "}
+        {/* added mt-12 for spacing */}
         <div className="h-px w-full absolute inset-x-0 bg-gradient-to-b from-neutral-300/50 via-neutral-200 to-transparent pointer-events-none"></div>
-
         <div className="max-w-7xl mx-auto">
           <Image
             src="/banner.webp"
