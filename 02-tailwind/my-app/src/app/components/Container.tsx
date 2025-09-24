dd0ff5a576169093dbec8df783e5c65d31416301
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-09-24 14:57:32 +0530

    latest next version

--- 02-tailwind/my-app/src/app/components/Container.tsx
+++ 02-tailwind/my-app/src/app/components/Container.tsx
@@ -8,9 +8,5 @@ export const Container = ({
   children: React.ReactNode;
   className: string;
 }) => {
-  return (
-    <div className={cn("max-w-7xl mx-auto px-4 md:py-4 relative z-10")}>
-      {children}
-    </div>
-  );
+  return <div className={cn("max-w-7xl mx-auto px-4 md:py-4")}>{children}</div>;
 };
--- 02-tailwind/my-app/src/app/page.tsx
+++ 02-tailwind/my-app/src/app/page.tsx
@@ -21,13 +21,13 @@ export default function Home() {
       <div className="relative w-full">
         {/* added mt-12 for spacing */}
         <div className="h-px w-full absolute inset-x-0 bg-gradient-to-b from-neutral-300/50 via-neutral-200 to-transparent pointer-events-none"></div>
-        <div className="max-w-7xl mx-auto p-4">
+        <div className="max-w-7xl mx-auto p-4 relative z-0">
           <Image
             src="/banner.webp"
             alt="banner"
             width={1000}
             height={1000}
-            className="rounded-xl w-full object-cover object-left-top border-neutral-200 shadow-md mask-b-from-0% to-100%"
+            className="rounded-xl w-full object-cover object-left-top border-neutral-200 shadow-md"
           />
         </div>
       </div>
