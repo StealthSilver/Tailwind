c730f37a134f96be90269ecd4a0e53d57920a7de
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-09-24 13:57:20 +0530

    latest next version

--- 02-tailwind/my-app/src/app/page.tsx
+++ 02-tailwind/my-app/src/app/page.tsx
@@ -3,8 +3,15 @@ import { Navbar } from "@/app/components/Navbar";
 
 export default function Home() {
   return (
-    <Container className="">
-      <Navbar></Navbar>
-    </Container>
+    <div className="h-screen relative [background:radial-gradient(125%_100%_at_50%_0%,_#FFF_6.32%,_#E0F0FF_29.28%,_#E7EFFD_68.68%,_#FFF_100%)]">
+      <div className="max-w-7xl mx-auto absolute inset-0 h-full w-full">
+        <div className="absolute inset-y-0 left-0 h-full w-px bg-gradient-to-b from-neutral-300/50 via-neutral-200 to-transparent"></div>
+
+        <div className="absolute inset-y-0 right-0 h-full w-px bg-gradient-to-b from-neutral-300/50 via-neutral-200 to-transparent"></div>
+      </div>
+      <Container className="">
+        <Navbar></Navbar>
+      </Container>
+    </div>
   );
 }
