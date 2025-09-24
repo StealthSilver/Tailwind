73fd3bab9c1f5a1739711d105c641df3ca26eb5b
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-09-24 14:55:49 +0530

    latest next version

--- 02-tailwind/my-app/src/app/page.tsx
+++ 02-tailwind/my-app/src/app/page.tsx
@@ -7,26 +7,27 @@ export default function Home() {
   return (
     <div className="h-screen relative [background:radial-gradient(125%_100%_at_50%_0%,_#FFF_6.32%,_#E0F0FF_29.28%,_#E7EFFD_68.68%,_#FFF_100%)]">
       {/* left and right lines should be relative to the full screen, not container */}
-      <div className="absolute inset-y-0 left-0 h-full w-px bg-gradient-to-b from-neutral-300/50 via-neutral-200 to-transparent pointer-events-none"></div>
-      <div className="absolute inset-y-0 right-0 h-full w-px bg-gradient-to-b from-neutral-300/50 via-neutral-200 to-transparent pointer-events-none"></div>
 
+      <div className="absolute inset-0 h-full w-full">
+        <div className="absolute inset-y-0 left-0 h-full w-px bg-gradient-to-b from-neutral-300/50 via-neutral-200 to-transparent pointer-events-none z-10"></div>
+        <div className="absolute inset-y-0 right-0 h-full w-px bg-gradient-to-b from-neutral-300/50 via-neutral-200 to-transparent pointer-events-none z-10"></div>
+      </div>
       <Container className={""}>
         <Navbar />
         <Hero />
       </Container>
 
       {/* horizontal line + banner */}
-      <div className="relative w-full mt-12">
-        {" "}
+      <div className="relative w-full">
         {/* added mt-12 for spacing */}
         <div className="h-px w-full absolute inset-x-0 bg-gradient-to-b from-neutral-300/50 via-neutral-200 to-transparent pointer-events-none"></div>
-        <div className="max-w-7xl mx-auto">
+        <div className="max-w-7xl mx-auto p-4">
           <Image
             src="/banner.webp"
             alt="banner"
             width={1000}
             height={1000}
-            className="rounded-2xl w-full object-cover object-left-top"
+            className="rounded-xl w-full object-cover object-left-top border-neutral-200 shadow-md mask-b-from-0% to-100%"
           />
         </div>
       </div>
