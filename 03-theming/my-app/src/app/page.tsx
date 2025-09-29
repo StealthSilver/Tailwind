8315cc3621c55286b9056172c584b1a6b1433bd8
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-09-29 14:52:09 +0530

    theming

--- 03-theming/my-app/src/app/page.tsx
+++ 03-theming/my-app/src/app/page.tsx
@@ -1,8 +1,5 @@
-import { Container } from "@/app/components/Container";
-import { Navbar } from "@/app/components/Navbar";
-import { Hero } from "./components/Hero";
 import Image from "next/image";
 
 export default function Home() {
-  return <></>;
+  return <>Helloo</>;
 }
