3124834a8429c05105c0541218beeb914e7c248b
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-10-01 13:50:06 +0530

    tailwind utility clases

--- 04-responsive/my-app/src/app/components/Hero.tsx
+++ 04-responsive/my-app/src/app/components/Hero.tsx
@@ -10,6 +10,13 @@ export const Hero = () => {
       <div className="sm:text-2xl md:text-3xl lg:text-5xl xl:text-7xl 2xl:text-9xl">
         This is responsive
       </div>
+
+      {/* using the flex properties for responsive alignment */}
+      <div className="flex flex-col md:flex-row items-center gap-10">
+        <div className="h-40 w-40 rounded-xl bg-red-300"></div>
+        <div className="h-40 w-40 rounded-xl bg-green-300"></div>
+        <div className="h-40 w-40 rounded-xl bg-blue-300"></div>
+      </div>
     </div>
   );
 };
